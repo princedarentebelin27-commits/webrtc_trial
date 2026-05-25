5da7557cb5037c1ef35162e940d40132f8598c13
princedarentebelin27-commits <princedarentebelin27@gmail.com>
2026-05-25 13:00:07 +0800

    Update webrtc_cam.py

--- webrtc_cam.py
+++ webrtc_cam.py
@@ -10,6 +10,6 @@ class VideoProcessor:
         edging = cv2.cvtColor(cv2.Canny(image, 100, 100), cv2.COLOR_GRAY2BGR)
         return av.VideoFrame.from_ndarray(edging,format = "bgr24")
     
-webrtc_streamer(key = "key",  rtc_configuration={
+webrtc_streamer(key = "key",  video_processor_factory= VideoProcessor, rtc_configuration={
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    },video_processor_factory= VideoProcessor)
+    })
