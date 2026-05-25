ffb056b23a3d1cea165c69156c229d20e91fe242
princedarentebelin27-commits <princedarentebelin27@gmail.com>
2026-05-25 12:34:54 +0800

    Add files via upload

---
+++ webrtc_cam.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import cv2
+from streamlit_webrtc import webrtc_streamer 
+import numpy as np
+import av 
+
+class VideoProcessor:
+    def recv(self,frame):
+        image = frame.to_ndarray(format = "bgr24")
+        edging = cv2.cvtColor(cv2.Canny(image, 100, 100), cv2.COLOR_GRAY2BGR)
+        return av.VideoFrame.from_ndarray(edging,format = "bgr24")
+    
+webrtc_streamer(key = "key",video_processor_factory= VideoProcessor)
